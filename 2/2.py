5545dc1dc9096ad102c749345e164fbf9f42a353
yukseltron <h.yuksel04@gmail.com>
2020-12-02 22:55:43 -0500

    2 done

--- 2/2.py
+++ 2/2.py
@@ -19,8 +19,19 @@ length = len(lists)
 
 for l in range(length):
     a = lists[l].count(letters[l])
+    string = lists[l]
+    p1 = ranges[l][0]
+    p2 = ranges[l][1]
+    print(string[int(p1)],p1,string[int(p2)], p2, letters[l],lists[l], string[int(p1)] == letters[l] or string[int(p2)] == letters[l])
+    if ((string[int(p1)] == letters[l]) ^ (string[int(p2)] == letters[l])):
+        counter += 1
+
+#pt1
+'''
+for l in range(length):
+    a = lists[l].count(letters[l])
     print(ranges[l],letters[l], a, lists[l], a >= int(ranges[l][0]) and a <= int(ranges[l][1]))
     if (a >= int(ranges[l][0]) and a <= int(ranges[l][1])):
         counter += 1
-
+'''
 print(counter)
