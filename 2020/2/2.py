31259b2f2c666c23d382f0ef9a9fb550572cc90a
yukseltron <h.yuksel04@gmail.com>
2020-12-03 14:42:44 -0500

    reorganized

---
+++ 2020/2/2.py
@@ -0,0 +1,39 @@
+f = open("input.txt", "r")
+lists = []
+letters = []
+ranges = []
+length = 0
+counter = 0
+
+
+for x in f:
+    a = x.split(":")
+    b = a[0].split(" ")
+    c = b[0].split("-")
+
+    lists.append(a[1])
+    letters.append(b[1])
+    ranges.append(c)
+
+length = len(lists)
+
+#pt1
+'''
+for l in range(length):
+    a = lists[l].count(letters[l])
+    print(ranges[l],letters[l], a, lists[l], a >= int(ranges[l][0]) and a <= int(ranges[l][1]))
+    if (a >= int(ranges[l][0]) and a <= int(ranges[l][1])):
+        counter += 1
+'''
+
+#pt2
+for l in range(length):
+    a = lists[l].count(letters[l])
+    string = lists[l]
+    p1 = ranges[l][0]
+    p2 = ranges[l][1]
+    print(string[int(p1)],p1,string[int(p2)], p2, letters[l],lists[l], string[int(p1)] == letters[l] or string[int(p2)] == letters[l])
+    if ((string[int(p1)] == letters[l]) ^ (string[int(p2)] == letters[l])):
+        counter += 1
+
+print(counter)
