0274be583948a97c0c16a8f83fd3fadce51159ad
yukseltron <h.yuksel04@gmail.com>
2020-12-02 22:28:49 -0500

    finished 2

--- 2/2.py
+++ 2/2.py
@@ -1,8 +1,26 @@
 f = open("input.txt", "r")
-list = []
+lists = []
+letters = []
+ranges = []
+length = 0
+counter = 0
+
 
 for x in f:
-    print(x)
-    #list.append
+    a = x.split(":")
+    b = a[0].split(" ")
+    c = b[0].split("-")
 
+    lists.append(a[1])
+    letters.append(b[1])
+    ranges.append(c)
 
+length = len(lists)
+
+for l in range(length):
+    a = lists[l].count(letters[l])
+    print(ranges[l],letters[l], a, lists[l], a >= int(ranges[l][0]) and a <= int(ranges[l][1]))
+    if (a >= int(ranges[l][0]) and a <= int(ranges[l][1])):
+        counter += 1
+
+print(counter)
